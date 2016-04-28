aa79a24e4a1f965e9fec2c3d189917bb180e9626
santiaago <santiago.ariassar@gmail.com>
2016-04-28 22:21:13 +0100

    simple web sockets example in go and react

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/websocket"
 )
 
 func init() {
 	log.SetFlags(log.Ltime | log.Ldate | log.Lshortfile)
 }
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+func EchoServer(ws *websocket.Conn) {
+	io.Copy(ws, ws)
 }
 
 func main() {
 	log.Println("Starting server")
 
-	http.HandleFunc("/", helloHandler)
+	http.Handle("/", http.FileServer(http.Dir("./web/")))
+
+	http.Handle("/echo", websocket.Handler(EchoServer))
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServer", err)
 	}
---
+++ web/app.jsx
@@ -0,0 +1,58 @@
+var Root = React.createClass({
+
+	getInitialState: function(){
+		return {
+			messages: [],
+		};
+	},
+
+	 componentDidMount: function() { 
+		 this.serverSocket = new WebSocket("ws://localhost:8080/echo");
+		 this.serverSocket.onopen = function() {
+			 this.serverSocket.send("Connection init");
+		 }.bind(this);
+
+		 this.serverSocket.onmessage = function(e) {
+			 var messages = this.state.messages;
+			 messages.push("Received: " + e.data);
+			 this.setState({messages: messages});
+		 }.bind(this);
+	 },
+	
+	
+	sendData: function(e){
+		
+		if (e && e.keyCode === 13) {
+			var data = this.refs.userInput.getDOMNode().value;
+			console.log(data);
+
+			this.serverSocket.send(data);
+			 var messages = this.state.messages;
+			this.state.messages.push("Sent: " + data);
+			 this.setState({messages: messages});
+		}
+		return;
+	},
+	
+	render: function(){
+
+		var messages = this.state.messages.map(function (message) {
+			return (
+				<div>{message}</div>
+			);
+		}, this);
+
+		
+		return (
+			<div className="root-container">
+				<input id="sendtext" ref="userInput" type="text" onKeyDown={this.sendData} />
+				{messages}
+			</div>
+		);
+	}
+});
+
+React.render(
+	<Root />,
+	document.getElementById('app-container')
+);
