83f5a54963567de3675f5d8dd677bfc34aa87e7b
santiaago <santiago.ariassar@gmail.com>
2016-04-28 21:10:38 +0100

    hello world server

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,7 +10,14 @@ func init() {
 	log.SetFlags(log.Ltime | log.Ldate | log.Lshortfile)
 }
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world")
+}
+
 func main() {
+	log.Println("Starting server")
+
+	http.HandleFunc("/", helloHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServer", err)
 	}
